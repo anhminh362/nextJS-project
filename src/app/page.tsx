cb8ecbd1f554c5a14a371c0619ce7b1c961847be
anhminh362 <116275383+anhminh362@users.noreply.github.com>
2023-08-06 18:31:09 +0700

    click to change img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
+"use client";
 import Image from "next/image";
 import Button from "../../ui/button";
 import Link from "next/link";
+import { useState } from "react";
 
+const img = [
+  "https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fbag-1-dark.png%3Fv%3D1689796304&w=1200&q=75",
+  "https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fbag-1-light.png%3Fv%3D1689796304&w=1200&q=75",
+];
 export default function Home() {
+  const [nextImg, setNextImg] = useState(img[0]);
+  const HandleNextImgClick = () => {
+    nextImg === img[0] ? setNextImg(img[1]) : setNextImg(img[0]);
+  };
+
   return (
     <div className="mx-auto max-w-screen-2xl px-4 ">
       <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row">
         <div className="h-full w-full basis-full lg:basis-4/6 ">
           <div className=" relative aspect-square overflow-hidden h-full max-h-[550px] w-full ">
             <img
-              src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fbag-1-light.png%3Fv%3D1689796304&w=1200&q=75"
+              src={nextImg}
               alt="white-bag"
               className="w-full h-full object-contain"
               style={{
@@ -26,8 +37,10 @@ export default function Home() {
             />
             <div className="absolute bottom-[15%]  flex w-full justify-center">
               <div className="flex rounded-full bg-neutral-50/80 backdrop-blur-sm text-neutral-500 items-center h-11 mx-auto border border-white">
-                <Link href={""}
-                className="h-full px-6 transition-all ease-in-out hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center">
+                <button
+                  onClick={HandleNextImgClick}
+                  className="h-full px-6 transition-all ease-in-out hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -43,10 +56,12 @@ export default function Home() {
                       d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
                     ></path>
                   </svg>
-                </Link>
+                </button>
                 <div className="mx-1 h-6 w-px bg-neutral-300"></div>
-                <Link href={""}
-                className="h-full px-6 transition-all ease-in-out hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center">
+                <button
+                  onClick={HandleNextImgClick}
+                  className="h-full px-6 transition-all ease-in-out hover:scale-110 hover:text-black dark:hover:text-white flex items-center justify-center"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -62,28 +77,50 @@ export default function Home() {
                       d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
                     ></path>
                   </svg>
-                </Link>
+                </button>
               </div>
             </div>
           </div>
           <ul className="my-12 flex items-center justify-center gap-2 overflow-auto py-1 lg:mb-0">
             <li className="h-auto w-20">
-              <Link href={''} className="w-full h-full">
-                <div className="w-full h-full bg-white overflow-hidden rounded flex justify-center border hover:border-blue-500">
-                  <img src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fbag-1-dark.png%3Fv%3D1689796304&w=96&q=75" alt="" className="ease-in-out hover:scale-105 relative h-full w-full object-contain transition " 
-                  style={{width:80,height:80,color:'transparent'}}
+              <button
+                onClick={() => setNextImg(img[1])}
+                className="w-full h-full"
+              >
+                <div
+                  className={`${
+                    nextImg === img[1]
+                      ? " border-blue-700 border-2"
+                      : " hover:border-blue-500"
+                  } w-full h-full bg-white overflow-hidden rounded flex justify-center border `}
+                >
+                  <img
+                    src={img[1]}
+                    alt=""
+                    className="ease-in-out hover:scale-105 relative h-full w-full object-contain transition "
+                    style={{ width: 80, height: 80, color: "transparent" }}
                   />
                 </div>
-              </Link>
+              </button>
             </li>
             <li className="h-auto w-20">
-              <Link href={''} className="w-full h-full">
-                <div className="w-full h-full bg-white overflow-hidden rounded flex justify-center border hover:border-blue-500">
-                  <img src="https://demo.vercel.store/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0754%2F3727%2F7491%2Ffiles%2Fbag-1-light.png%3Fv%3D1689796304&w=96&q=75" alt="" className="ease-in-out hover:scale-105 relative h-full w-full object-contain transition " 
-                  style={{width:80,height:80,color:'transparent'}}
+              <button
+                onClick={() => setNextImg(img[0])}
+                className="w-full h-full"
+              >
+                <div className={`${
+                    nextImg === img[0]
+                      ? " border-blue-700 border-2"
+                      : " hover:border-blue-500"
+                  } w-full h-full bg-white overflow-hidden rounded flex justify-center border `}>
+                  <img
+                    src={img[0]}
+                    alt=""
+                    className="ease-in-out hover:scale-105 relative h-full w-full object-contain transition "
+                    style={{ width: 80, height: 80, color: "transparent" }}
                   />
                 </div>
-              </Link>
+              </button>
             </li>
           </ul>
         </div>
@@ -99,14 +136,21 @@ export default function Home() {
           <div className="block">
             <h1 className=" uppercase mb-4 text-sm font-medium">Color</h1>
             <div className="flex flex-wrap gap-3 mb-6">
-              <button className="flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 py-1 px-2 text-base cursor-default ring-2 ring-blue-600">Black</button>
+              <button className="flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 py-1 px-2 text-base cursor-default ring-2 ring-blue-600">
+                Black
+              </button>
               <Button name={"White"} />
             </div>
           </div>
           <div className="block">
             <h1 className=" uppercase mb-4 text-sm font-medium">Size</h1>
             <div className="flex flex-wrap gap-3 mb-6">
-              <button disabled className="flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 py-1 px-2 text-base ring-neutral-300 ring-1 text-neutral-500">6 x 8 inch</button>
+              <button
+                disabled
+                className="flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 py-1 px-2 text-base ring-neutral-300 ring-1 text-neutral-500"
+              >
+                6 x 8 inch
+              </button>
               <Button name={"7 x 9 inch"} />
               {/* <button className="rounded-full border bg-neutral-100 hover:scale-110 hover:ring-2 hover:ring-blue-600 px-3 py-1 flex min-w-[48px] items-center justify-center text-sm  ring-1 ring-white  transition  ease-in-out">7 x 9 inch</button> */}
               <Button name={"8 x 11 inch"} />
